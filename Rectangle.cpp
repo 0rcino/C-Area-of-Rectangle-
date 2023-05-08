e177e15fb12f97c3c4b4a5e9eae098488d52806d
Orcino <94884406+0rcino@users.noreply.github.com>
2023-05-09 06:05:54 +0800

    Rectangle of Area in C++

---
+++ Rectangle.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std; 
+
+int main () {
+
+  double height, width;
+  double area;
+
+  cout << "Enter the Higth of Reactangle: ";
+  cin >> height;
+
+  cout << "Enter the Width of Reactangle: ";
+  cin >> width; 
+
+  area = height * width;
+
+  cout << "The Area of Rectangle is: " << area; 
+}
